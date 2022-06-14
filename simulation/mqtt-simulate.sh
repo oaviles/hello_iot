35d6b80357750ce82d2775e69a1dcfa0667a21ef
Omar Aviles <oaviles@live.com>
2022-06-14 11:21:15 +0000

    MQTT Scenario Added

--- simulation/mqtt-simulate.sh
+++ simulation/mqtt-simulate.sh
@@ -5,4 +5,4 @@ az login --service-principal -u $1 -p $2 --tenant $3
 
 az config set extension.use_dynamic_install=yes_without_prompt
 az iot hub device-identity create --device-id $DEVICEID --hub-name $IOTHUBNAME
-az iot device simulate -n $IOTHUBNAME -d $DEVICEID --protocol http
+az iot device simulate -n $IOTHUBNAME -d $DEVICEID --properties "level=storage;$.ct=application/json"
